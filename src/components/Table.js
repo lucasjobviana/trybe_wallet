8a203f472e02b1f506cfc5c0940c8238a43634a4
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-05-15 20:48:48 -0300

    Finalizando sétimo requisito.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
 
 class Table extends Component {
   headers = ['Descrição', 'Tag', 'Método de pagamento',
     'Valor', 'Moeda', 'Câmbio utilizado', 'Valor convertido',
     'Moeda de conversão', 'Editar/Excluir'];
 
+  // ['Cinco euros', 'Lazer', 'Cartão de crédito', '5.00', 'Euro/Real Brasileiro', '5.13', '25.63', 'Real'],
   render() {
+    const { expenses } = this.props;
+    console.log(expenses);
+
+    const linesTable = expenses.length > 0
+      ? expenses.map((expense, index) => (
+        <tr key={ index }>
+          <td>{expense.description}</td>
+          <td>{expense.tag}</td>
+          <td>{expense.method}</td>
+          <td>{Number.parseFloat(expense.value).toFixed(2) }</td>
+          <td>{expense.exchangeRates[expense.currency].name}</td>
+          <td>
+            {Number.parseFloat(expense.exchangeRates[
+              expense.currency].ask).toFixed(2) }
+
+          </td>
+          <td>
+            {(
+              Number.parseFloat(expense.exchangeRates[
+                expense.currency].ask)
+              * Number.parseFloat(expense.value)
+            ).toFixed(2)}
+
+          </td>
+          <td>Real</td>
+
+        </tr>)) : '';
+
     const headersElement = this.headers.map((header, index) => (
       <th key={ index }>{header}</th>));
 
@@ -13,11 +45,19 @@ class Table extends Component {
       <div>
         Table
         <table>
-          <tr>{headersElement}</tr>
+          <thead><tr>{headersElement}</tr></thead>
+          <tbody>{linesTable}</tbody>
+
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.shape().isRequired,
+};
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+export default connect(mapStateToProps)(Table);
