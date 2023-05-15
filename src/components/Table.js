cd92eb7c4470a5320106a848127b16ca39cde51c
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-05-15 19:01:48 -0300

    Finalizado sexto requisito.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
 class Table extends Component {
+  headers = ['Descrição', 'Tag', 'Método de pagamento',
+    'Valor', 'Moeda', 'Câmbio utilizado', 'Valor convertido',
+    'Moeda de conversão', 'Editar/Excluir'];
+
   render() {
+    const headersElement = this.headers.map((header, index) => (
+      <th key={ index }>{header}</th>));
+
     return (
-      <div>Table</div>
+      <div>
+        Table
+        <table>
+          <tr>{headersElement}</tr>
+        </table>
+      </div>
     );
   }
 }
