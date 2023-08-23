effb04e13ef5ab3b574a7b7a0b481d6dd834d487
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-08-23 20:49:05 -0300

    Adicionando dependencias

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/store';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
